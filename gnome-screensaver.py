77ec41a43553d5d86bd1b2a80c1168b525bfe672
Christopher Martin <ch.martin@gmail.com>
2012-03-10 21:11:19 -0500

    playing with gnome-screensaver: py/sh locking scripts

---
+++ gnome-screensaver.py
@@ -0,0 +1,35 @@
+# http://wiki.python.org/moin/DbusExamples
+
+# You must initialize the gobject/dbus support for threading before doing anything.
+import gobject
+gobject.threads_init()
+
+from dbus import glib
+glib.init_threads()
+
+# Create a session bus.
+import dbus
+bus = dbus.SessionBus()
+
+# Create an object that will proxy for a particular remote object.
+remote_object = bus.get_object(
+  "org.freedesktop.DBus", # Connection name
+  "/org/freedesktop/DBus" # Object's path
+)
+
+# Introspection returns an XML document containing information
+# about the methods supported by an interface.
+#print ("Introspection data:\n")
+#print remote_object.Introspect()
+
+iface = dbus.Interface(remote_object, 'org.freedesktop.DBus')
+#print iface.ListNames()
+
+screensaver = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+iface = dbus.Interface(screensaver, 'org.gnome.ScreenSaver')
+
+import sys
+arg = sys.argv[1]
+if arg == 'lock':
+  iface.Lock()
+
---
+++ gnome-screensaver.sh
@@ -0,0 +1,11 @@
+if [ "$1" == lock ]
+  then
+    dbus-send --session \
+              --dest=org.gnome.ScreenSaver \
+              --type=method_call \
+              --print-reply \
+              --reply-timeout=20000 \
+              /org/gnome/ScreenSaver \
+              org.gnome.ScreenSaver.Lock
+fi
+
