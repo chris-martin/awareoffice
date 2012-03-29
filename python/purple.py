8d0556174df36cbbc1d4c4b5893d53185ee6194b
Christopher Martin <ch.martin@gmail.com>
2012-03-29 14:17:50 -0400

    clarify whitespace

--- python/purple.py
+++ python/purple.py
@@ -23,8 +23,9 @@ class PurpleThread ( Thread ):
       select tmp from tmp_event
       where ts > ? and id = ?
     """, (
-      int((time.time() - 5) * 1000), self.id),
-    )
+      int((time.time() - 5) * 1000),
+      self.id
+    ))
     count = 0
     sum = 0
     for row in c:
